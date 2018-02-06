5d3dc0d697413c4e750534aac704bd9364a03300
anagtz82 <anagtz82@me.com>
2018-02-06 16:37:41 -0600

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+    let element = <h1 className="App-title">Welcome to Ana's React App</h1>;
+    return element;
+}
+
+ export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1 > Howdy, React! </h1>;
+}
+
+
+ReactDOM.render( < App /> , document.getElementById('root'));
